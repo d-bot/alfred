a25e7810d1b956998328914c5b8cfd1eb4c23be0
Dylan Choi <dchoi@djh.localdomain>
2017-05-02 21:51:02 +0000

    added naver real time search

--- alfred.py
+++ alfred.py
@@ -18,22 +18,26 @@ expire_at = expire_in + int(time.time())
 
 @app.route('/', methods=['GET', 'POST'])
 def alfred():
-
     if expire_at < int(time.time()):
         app.logger.info('Token has expired. Requesting new token')
-        y_token, expire_in = CacheToken().get_token()
-        app.logger.info('Received a new token: {} and it will expire in {}'.format(y_token, expire_in))
+    #    y_token, expire_in = CacheToken().get_token()
+    #    app.logger.info('Received a new token: {} and it will expire in {}'.format(y_token, expire_in))
+    else:
+        app.logger.info('Time left: {}'.format(expire_at-int(time.time())))
+
 
     if request.form['user_id'] != 'USLACKBOT':
         if re.search('추천', request.form['text']):
             r = '뭘 추천해달라는거고 미친놈아'
         elif re.search('테스트', request.form['text']):
             r = '테스트 하지마라 새끼야'
+        elif re.search('실검', request.form['text']):
+            r = real_time_search_queries()
         elif re.match('^yelp\s+(\S+)\s+(.*)$', request.form['text']):
             # this is fucking ugly
             m = re.match('^yelp\s+(\S+)\s+(.*)$', request.form['text'])
             term, location = m.group(1), m.group(2)
-            name,url,review_count,rating = yelpbot(term, location, y_token)
+            name,url,review_count,rating = yelpbot(term, location)
             r = name + ' ' + url.split('?')[0] + ' ' + str(review_count) + '\n' + str(rating)
         else:
             r = test()
@@ -48,9 +52,22 @@ def test():
     r = ['어쩌라고', '하루 8시간 자라', '오락 그만해라', '책 좀 읽어라', '운동 좀 해라']
     return random.choice(r)
 
+def real_time_search_queries():
+    queries = re.findall('<span class="ah_k">(.*)</span>', requests.get('http://naver.com').text)[:20]
+    return "\n".join(queries)
 
-def yelpbot(term, location, token):
-    res = requests.get('https://api.yelp.com/v3/businesses/search?term={}&location={}'.format(term, location), headers={'Authorization':'Bearer {}'.format(token)})
+def yelpbot(term, location):
+    #app.logger.info('I am in yelpbot space with {}'.format(y_token))
+
+    if expire_at < int(time.time()):
+        app.logger.info('Token has expired. Requesting new token')
+        #y_token, expire_in = CacheToken().get_token()  # this makes problematic!!!!
+        app.logger.info('Received a new token: {} and it will expire in {}'.format(y_token, expire_in))
+    else:
+        app.logger.info('current token: {}'.format(y_token))
+
+    app.logger.info('token expiration check is done: {}'.format(y_token))
+    res = requests.get('https://api.yelp.com/v3/businesses/search?term={}&location={}'.format(term, location), headers={'Authorization':'Bearer {}'.format(y_token)})
 
     data = json.loads(res.text)
 
@@ -62,7 +79,7 @@ def yelpbot(term, location, token):
 
     if len(must_try) > 0:
         must_try.sort(key=lambda r: r['review_count'], reverse=True)
-        app.logger.info(must_try[0])
+        #app.logger.info(must_try[0])
         restaurants = [Restaurant(res['name'], res['url'], res['review_count'], res['rating']) for res in must_try]
         return (restaurants[0].name, restaurants[0].url, restaurants[0].review_count, restaurants[0].rating)
     else:
@@ -70,7 +87,7 @@ def yelpbot(term, location, token):
 
 
 if __name__ == '__main__':
-    handler = RotatingFileHandler('logs/alfred.log', maxBytes=10000, backupCount=1)
+    handler = RotatingFileHandler('logs/alfred.log', maxBytes=100000000, backupCount=1)
     handler.setLevel(logging.INFO)
     app.logger.setLevel(logging.INFO)
     app.logger.addHandler(handler)
