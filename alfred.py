7e053691e6243fcc4e9029aaee9bd5d0e014d1d2
Dylan <choiza@gmail.com>
2017-05-07 09:09:28 +0000

    separated out yelp module from flask

--- alfred.py
+++ alfred.py
@@ -2,31 +2,18 @@ import logging
 from logging.handlers import RotatingFileHandler
 import random
 import re
-import json
-import time
-from collections import namedtuple
 
 import requests
-
 from flask import Flask, request, Response
 
 from cache_token import CacheToken
 from endic import search_endic
+from yelp_bot import YelpBot
 
 app = Flask(__name__)
 
-y_token, expire_in = CacheToken().get_token()
-expire_at = expire_in + int(time.time())
-
 @app.route('/', methods=['GET', 'POST'])
 def alfred():
-    if expire_at < int(time.time()):
-        app.logger.info('Token has expired. Requesting new token')
-    #    y_token, expire_in = CacheToken().get_token()
-    #    app.logger.info('Received a new token: {} and it will expire in {}'.format(y_token, expire_in))
-    #else:
-    #    app.logger.info('Time left: {}'.format(expire_at-int(time.time())))
-
 
     if request.form['user_id'] != 'USLACKBOT':
         if re.search('추천', request.form['text']):
@@ -39,19 +26,18 @@ def alfred():
             m = re.match('^e\s+(.*)$', request.form['text'])
             r = search_endic(m.group(1))
         elif re.search('^help', request.form['text']):
-            r = '''e [영어단어] : 네이버 사전에서 영어 단어 검색
-yelp [음식종류] near [도시/위치] : 옐프 top 3 레스토랑 검색
-실검 : 네이버 현재 실시간 상위 검색어들
-            '''
-        elif re.search('yelp', request.form['text']):
-            m = re.match('^yelp\s+(.*)\s+(near|in)\s+(.*)$', request.form['text'])
+            r = alfred_help()
+        elif re.search('y\s+', request.form['text']):
+            m = re.match('^y\s+(.*)\s+(near|in)\s+(.*)$', request.form['text'])
             term, location = m.group(1), m.group(3)
             try:
-                r = yelpbot(term, location)
+                ybot = YelpBot()
+                ybot.check_token_validity()
+                r = ybot.call_search_api(term, location)
             except Exception as e:
-                r = '*Sorry Yelp-bot couldn\'t process your request:* {} '.format(type(e)) + str(e)
+                r = '*Sorry YelpBot couldn\'t process your request:* {} '.format(type(e)) + str(e)
         else:
-            r = test()
+            r = random_resp()
 
         text = '{{"text":"{}"}}'.format(r)
         resp = Response(response=text, status=200, mimetype="application/json")
@@ -59,43 +45,16 @@ yelp [음식종류] near [도시/위치] : 옐프 top 3 레스토랑 검색
     else:
         return 'ok'
 
-def test():
+def random_resp():
     r = ['어쩌라고', '하루 8시간 자라', '오락 그만해라', '책 좀 읽어라', '운동 좀 해라']
     return random.choice(r)
+
+def alfred_help():
+    return 'e [영어단어] : 네이버 사전에서 영어 단어 검색\nyelp [음식종류] near [도시/위치] : 옐프 top 3 레스토랑 검색\n 실검 : 네이버 현재 실시간 상위 검색어들'
 
 def real_time_search_queries():
     queries = re.findall('<span class="ah_k">(.*)</span>', requests.get('http://naver.com').text)[:20]
     return "[ *현재 네이버 실시간 검색 순위* ]\n" + "\n".join(queries)
-
-def check_token_validity():
-    if expire_at < int(time.time()):
-        app.logger.info('Token has expired. Requesting new token')
-        #y_token, expire_in = CacheToken().get_token()  # this makes problematic!!!!
-        app.logger.info('Received a new token: {} and it will expire in {}'.format(y_token, expire_in))
-    #else:
-        #app.logger.info('current token looks ok: {}'.format(y_token))
-
-
-def yelpbot(term, location):
-    check_token_validity()
-    res = requests.get('https://api.yelp.com/v3/businesses/search?term={}&location={}'.format(term, location), headers={'Authorization':'Bearer {}'.format(y_token)})
-    data = json.loads(res.text)
-    Restaurant = namedtuple('Restaurant', 'name, url, review_count, rating')
-    must_try = [restaurant for restaurant in data['businesses'] if restaurant['rating'] > 3.4 and restaurant['review_count'] > 300]
-
-    if len(must_try) > 0:
-        must_try.sort(key=lambda r: r['review_count'], reverse=True)
-        restaurants = [Restaurant(res['name'], res['url'], res['review_count'], res['rating'])
-                for res in must_try][:3]
-
-        res_output = ''
-        for res in restaurants:
-            res_output = res_output + " *[ {} ]*\n{}\nReview Count: {}\nRating: {}\n\n".format(res.name, res.url.split('?')[0], res.review_count, res.rating)
-
-        return res_output
-
-    else:
-        return 'No search result!'
 
 
 if __name__ == '__main__':
@@ -105,23 +64,3 @@ if __name__ == '__main__':
     app.logger.addHandler(handler)
     app.run(host='0.0.0.0', port=5007, debug=True)
 
-
-
-'''
-{
-    'coordinates': {'longitude': -121.995439343154, 'latitude': 37.382495134431},
-    'categories': [{'title': 'American (Traditional)', 'alias': 'tradamerican'}, {'title': 'Burgers', 'alias': 'burgers'}, {'title': 'Sports Bars', 'alias': 'sportsbars'}],
-    'display_phone': '(408) 738-8515',
-    'is_closed': False,
-    'location': {'address2': 'Ste 110', 'address3': '', 'city': 'Sunnyvale', 'zip_code': '94085', 'display_address': ['510 Lawrence Expy', 'Ste 110', 'Sunnyvale, CA 94085'], 'address1': '510 Lawrence Expy', 'country': 'US', 'state': 'CA'},
-    'id': 'st-johns-bar-and-grill-sunnyvale-2',
-    'review_count': 1944,
-    'phone': '+14087388515',
-    'transactions': [],
-    'name': "St. John's Bar & Grill",
-    'image_url': 'https://s3-media1.fl.yelpcdn.com/bphoto/yc2i37ilxxS6am6Gbdpqzw/o.jpg',
-    'rating': 4.0, 'distance': 2734.1896414059997,
-    'url': 'https://www.yelp.com/biz/st-johns-bar-and-grill-sunnyvale-2?adjust_creative=0-LllWs1Yqig2LM8S38itQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=0-LllWs1Yqig2LM8S38itQ',
-    'price': '$$'
-}
-'''
---
+++ yelp_bot.py
@@ -0,0 +1,46 @@
+from cache_token import CacheToken
+import requests
+import time
+import json
+from collections import namedtuple
+
+
+class YelpBot:
+    def __init__(self):
+        self.y_token, self.expire_in = CacheToken().get_token()
+        self.expire_at = self.expire_in + int(time.time())
+
+    def check_token_validity(self):
+        if self.expire_at < int(time.time()):
+            # How do I want to get logger object here? I'll think about it later for now...
+            #app.logger.info('Token has expired. Requesting new token')
+            self.y_token, self.expire_in = CacheToken().get_token()
+            #app.logger.info('Received a new token: {} and it will expire in {}'.format(self.y_token, expire_in))
+        #else:
+            #app.logger.info('current token looks ok: {}'.format(self.y_token))
+
+
+    def call_search_api(self, term, location):
+        res = requests.get('https://api.yelp.com/v3/businesses/search?term={}&location={}'.format(term, location), headers={'Authorization':'Bearer {}'.format(self.y_token)})
+        data = json.loads(res.text)
+        Restaurant = namedtuple('Restaurant', 'name, url, review_count, rating')
+        must_try = [restaurant for restaurant in data['businesses'] if restaurant['rating'] > 3.4 and restaurant['review_count'] > 300]
+
+        if len(must_try) > 0:
+            must_try.sort(key=lambda r: r['review_count'], reverse=True)
+            restaurants = [Restaurant(res['name'], res['url'], res['review_count'], res['rating'])
+                    for res in must_try][:3]
+
+            res_output = ''
+            for res in restaurants:
+                res_output = res_output + " *[ {} ]*\n{}\nReview Count: {}\nRating: {}\n\n".format(res.name, res.url.split('?')[0], res.review_count, res.rating)
+
+            return res_output
+
+        else:
+            return 'No search result!'
+
+if __name__ == '__main__':
+    ybot = YelpBot()
+    ybot.check_token_validity()
+    print(ybot.call_search_api('burrito', 'santa clara'))
